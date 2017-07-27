6fd5cceb6ba6f3e9a20e61ed6312c01a8d12c46f
OSS2017 <young@aurora>
2017-07-26 22:22:59 -0700

    Calculation to normalize Bridging variables and create a bridging csv

---
+++ bridging_calc.R
@@ -0,0 +1,32 @@
+# Calculation of Social Capital Bridging Values
+
+#Load bridging calculation raw data
+
+bridge_var <- read.csv("bridging_variables.csv")
+
+
+#normalization of yearly data
+
+SoCI_bridge_stats <- bridge_var %>% group_by(year) %>% 
+  summarize_at(vars(Religion:Civic), c("mean", "max", "min"), na.rm = TRUE)
+
+
+SoCI_bridge_combo <- left_join(bridge_var, SoCI_bridge_stats, by = "year")
+
+SoCI__bridge_norm <- SoCI_bridge_combo %>% mutate (religion_soc = (Religion - Religion_min)/(Religion_max - Religion_min),
+                                        civic_soc = (Civic -Civic_min)/(Civic_max - Civic_min),
+                                        affiliate_soc = (Affiliate - Affiliate_min)/(Affiliate_max - Affiliate_min))
+
+
+SoCI_bridge_2 <- mutate(SoCI__bridge_norm, SoCI_bridge = ((religion_soc + civic_soc + 
+                                                 affiliate_soc)/3))
+
+View(SoCI_bridge_2)
+
+SoCI_bridge_2010 <- select(SoCI_bridge_2, c(fips_n, year, SoCI_bridge))
+
+View(SoCI_bridge_2010)
+
+
+# write final csv
+write.csv(SoCI_bond_final, file = "SoCI_bond_final.csv")
