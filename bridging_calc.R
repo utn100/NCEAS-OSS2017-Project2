205809e129709d8164aadd9a8f7290d391a50fed
OSS2017 <young@aurora>
2017-07-27 08:37:34 -0700

    Normalization of bridging data; included comments to describe what the code is doing

--- bridging_calc.R
+++ bridging_calc.R
@@ -5,13 +5,15 @@
 bridge_var <- read.csv("bridging_variables.csv")
 
 
-#normalization of yearly data
+# Get statistics for the variables by year
 
 SoCI_bridge_stats <- bridge_var %>% group_by(year) %>% 
   summarize_at(vars(Religion:Civic), c("mean", "max", "min"), na.rm = TRUE)
 
 
 SoCI_bridge_combo <- left_join(bridge_var, SoCI_bridge_stats, by = "year")
+
+#normalization of yearly data
 
 SoCI__bridge_norm <- SoCI_bridge_combo %>% mutate (religion_soc = (Religion - Religion_min)/(Religion_max - Religion_min),
                                         civic_soc = (Civic -Civic_min)/(Civic_max - Civic_min),
@@ -21,7 +23,7 @@ SoCI__bridge_norm <- SoCI_bridge_combo %>% mutate (religion_soc = (Religion - Re
 SoCI_bridge_2 <- mutate(SoCI__bridge_norm, SoCI_bridge = ((religion_soc + civic_soc + 
                                                  affiliate_soc)/3))
 
-View(SoCI_bridge_2)
+# Pull out final table with just fips, year and bridging number
 
 SoCI_bridge_2010 <- select(SoCI_bridge_2, c(fips_n, year, SoCI_bridge))
 
@@ -29,4 +31,4 @@ View(SoCI_bridge_2010)
 
 
 # write final csv
-write.csv(SoCI_bond_final, file = "SoCI_bond_final.csv")
+write.csv(SoCI_bridge_2010, file = "SoCI_bridge_2010.csv")
